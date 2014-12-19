fcb2511797a5592338fa6adca1f3a5b36cb1920b
umehrot2 <umehrot2@illinois.edu>
2014-12-20 03:07:42 +0530

    UnionFind implemented

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -24,16 +24,25 @@ public class UnionFind {
 
     public void union(int p, int q)
     {
-        
+        int i = arr[p];
+        int j = arr[q];
+
+        for(int loop = 0; loop < arr.length; loop++)
+        {
+            if(arr[loop] == i)
+            {
+                arr[loop] = j;
+            }
+        }
     }
 
     public static void main(String[] args)
     {
         int n;
-
-        if(args[0] != null)
+        if(args.length != 0)
         {
             n = Integer.parseInt(args[0]);
+            if(n < 10) n = 10;
         }
         else
         {
@@ -41,6 +50,33 @@ public class UnionFind {
         }
 
         UnionFind uf = new UnionFind(n);
+
+
+        //List of unions on numbers less than 10
+        uf.union(1,3);
+        uf.union(1,5);
+        uf.union(3,4);
+        uf.union(5,7);
+        uf.union(2,9);
+
+        //Check for connected components
+        System.out.println("Checking connected components : ");
+        System.out.println(uf.connected(1,3));
+        System.out.println(uf.connected(1,4));
+        System.out.println(uf.connected(1,7));
+        System.out.println(uf.connected(1,8));
+        System.out.println(uf.connected(1,9));
+
+        System.out.println("\nApplying union commands\n");
+        uf.union(7,8);
+        uf.union(8,9);
+
+        System.out.println("Checking connected components after unions : ");
+        System.out.println(uf.connected(1,8));
+        System.out.println(uf.connected(1,9));
+        System.out.println(uf.connected(2,1));
+        System.out.println(uf.connected(6,9));
+
 
     }
 }
