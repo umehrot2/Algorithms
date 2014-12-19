66274dd870316f277bde1c6644aaf92788888eed
sharma55_umehrot2 <abhinavsharma3105@gmail.com>
2014-12-20 02:23:00 +0530

    First Commit

---
+++ src/UnionFind.java
@@ -0,0 +1,33 @@
+/**
+ * Created by uditmehrotra on 20/12/14.
+ */
+public class UnionFind {
+
+    int arr[];
+
+    private UnionFind(int n)
+    {
+        arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = i;
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        int n;
+
+        if(args[0] != null)
+        {
+            n = Integer.parseInt(args[0]);
+        }
+        else
+        {
+            n = 10;
+        }
+
+        UnionFind uf = new UnionFind(n);
+
+    }
+}
